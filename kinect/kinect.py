57eff165ebd0cae0361354a5ddcd6bf94e499422
Robaire Galliath <robaire@galliath.com>
2019-01-19 15:06:28 -0500

    Update

--- kinect/kinect.py
+++ kinect/kinect.py
@@ -12,38 +12,70 @@ if sys.hexversion >= 0x03000000:
 else:
 	import thread
 
-class runtime(object):
+class Game():
 	def __init__(self):
-		# Kinect runtime object, we want only color and body frames 
-		self._kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Body)
+		pygame.init()   # Initialize Pygame
 
-		# here we will store skeleton data 
-		self._bodies = None
+		# Set the width and height of the screen [width, height]
+		self._infoObject = pygame.display.Info()
+		self._screen = pygame.display.set_mode(
+			(self._infoObject.current_w >> 1, self._infoObject.current_h >> 1),
+			pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE, 32)
 
-	def run(self):
-		while(True):
-			# -------- Main Program Loop -----------
+		pygame.display.set_caption("Kinect Game")  # Set the title of the window
 
-			# --- Cool! We have a body frame, so can get skeletons
-			if self._kinect.has_new_body_frame(): 
-				self._bodies = self._kinect.get_last_body_frame()
+		# Loop until the user clicks the close button.
+		self._done = False
 
-			# --- draw skeletons to _frame_surface
-			if self._bodies is not None: 
-				for i in range(0, self._kinect.max_body_count):
-					body = self._bodies.bodies[i]
-					if not body.is_tracked: 
-						continue 
+		# Used to manage how fast the screen updates
+		self._clock = pygame.time.Clock()
 
-					joints = body.joints 
-					joint_points = self._kinect.body_joints_to_color_space(joints)
+        # Kinect runtime object, we want only color and body frames 
+		self._kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Color | PyKinectV2.FrameSourceTypes_Body)
 
-					print("x = ", joint_points[11].x) #get x
-					print("y = ", joint_points[11].y) #get y
-					
-					RightHandState = body.hand_right_state #get hand state
-					print(RightHandState);
+        # back buffer surface for getting Kinect color frames, 32bit color, width and height equal to the Kinect color frame size
+		self._frame_surface = pygame.Surface((self._kinect.color_frame_desc.Width, self._kinect.color_frame_desc.Height), 0, 32)
+
+		# Create a Kinect Data object to position information
+		self.kinect_data = KinectData(self._kinect)
+
+	def game_loop(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+
+				# Other events as needed
+			
+			self.update()
+
+	def update(self):
+		print(self.kinect_data.get_hand_data())
+
+
+class KinectData():
+
+	def __init__(self, kinect):
+
+		self._kinect = kinect  # Kinect Runtime Object
+		self._bodies = None  # Stores skeleton position data
+
+	def get_hand_data(self):
+		""" Retrives the positional data of the players right hand """
+		if self._kinect.has_new_body_frame():  # Check if there is a new body frame
+			self._bodies = self._kinect.get_last_body_frame()
+
+		if self._bodies is not None:  # If there is a body in the frame
+			for i in range(0, self._kinect.max_body_count):
+				body = self._bodies.bodies[i]
+				if not body.is_tracked: 
+					continue
+
+				joint_points = self._kinect.body_joints_to_color_space(body.joints)
+
+				return [joint_points[11].x, joint_points[11].y, body.hand_right_state]
 	
-__main__ = "Kinect Trackinig"
-game = runtime();
-game.run();
+__main__ = "Kinect Tracking"
+
+game = Game()
+game.game_loop()
