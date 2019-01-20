6aadfbbc7f63f90b4051d57b719aa1c0be7c3e1c
Teddy Koker <teddy.koker@gmail.com>
2019-01-19 19:16:44 -0500

    asdf

--- run.py
+++ run.py
@@ -1,11 +1,35 @@
 import asyncio
 import websockets
+import threading
+import queue
+
+q = queue.Queue()
 
 
-async def hello(uri):
+uri = "ws://localhost:8080/websocket"
+
+
+async def receiver():
     async with websockets.connect(uri) as websocket:
         while True:
-            print(await websocket.recv())
+            q.put(await websocket.recv())
 
 
-asyncio.get_event_loop().run_until_complete(hello("ws://localhost:8080/websocket"))
+def loop_in_thread(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(receiver())
+
+
+def run_web_socket():
+    loop = asyncio.get_event_loop()
+    t = threading.Thread(target=loop_in_thread, args=(loop,))
+    t.start()
+
+
+######################################### ROBAIRE PUT THIS IN THE GAME
+run_web_socket()
+while True:
+    if not q.empty():
+        print(q.get())
+
+###########################################
